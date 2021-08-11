216b474356788d16f89760707e10faba119cfbbe
zhutao09 <13221019673@163.com>
2021-08-11 13:35:00 +0800

    hot-fix commit

--- src/main/java/Test/Test.java
+++ src/main/java/Test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args){
         System.out.println("git-test");
         System.out.println("hello word");
+        System.out.println("git-test hot-fix");
     }
 }
